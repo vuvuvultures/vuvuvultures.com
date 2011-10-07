402cbc794f56374f57603fd0bf90833eb8d31b9d
Gareth Visagie <gareth.visagie@gmail.com>
2011-10-07 11:39:35 +0100

    Still trying to get heroku to play nicely

--- application.rb
+++ application.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  cache_control :public, :must_revalidate, :max_age => 86400
-  File.read(File.join(settings.public, 'index.html'))
+#  cache_control :public, :must_revalidate, :max_age => 86400
+#  File.read(File.join(settings.public, 'index.html'))
+"hello world"
 end
