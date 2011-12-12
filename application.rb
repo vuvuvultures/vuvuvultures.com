fccb585b112d78750bbb7d6d80154123acf626ba
Gareth Visagie <gareth.visagie@gmail.com>
2011-12-12 22:05:31 +0000

    Better caching across all static files

--- application.rb
+++ application.rb
@@ -3,9 +3,10 @@ require 'sinatra'
 
 get '/' do
   cache_control :public, :must_revalidate, :max_age => 86400
-  File.read('./public/index.html')
+  send_file './public/index.html'  
 end
 
-get %r{^/vvvep.zip/?$}i do
+get %r{^/1vvvep.zip/?$}i do
+  cache_control :public, :must_revalidate, :max_age => 86400
   send_file './public/VVVEP.zip'
 end
