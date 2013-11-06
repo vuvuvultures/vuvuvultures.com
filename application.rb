b9e8aef877bdfe206210ebb3d12b974ec1d5780b
Gareth Visagie <gareth@gjvis.com>
2013-11-06 21:22:42 +0000

    Manually serve downloadpushpull.html for requests to /downloadpushpull

--- application.rb
+++ application.rb
@@ -6,6 +6,11 @@ get '/' do
   send_file './public/index.html'  
 end
 
+get %r{^/downloadpushpull/?$}i do
+  cache_control :public, :must_revalidate, :max_age => 86400
+  send_file './public/downloadpushpull.html'
+end
+
 get %r{^/vvvep.zip/?$}i do
   cache_control :public, :must_revalidate, :max_age => 86400
   send_file './public/VVVEP.zip'
